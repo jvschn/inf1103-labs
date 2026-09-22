17d578bb02185ccfdf20941b505bc432627afaf6
Jovis <2601878@sit.singaporetech.edu.sg>
2026-09-22 22:33:06 +0800

    Fixed some errors and added $ to user output

--- Lab 3/modular_auditor.py
+++ Lab 3/modular_auditor.py
@@ -9,14 +9,12 @@ def get_valid_input(user_input):
     if user_input == "quit":
         return user_input
     elif user_input.isdigit():
-        if int(uinput) < 0:
-            error += 1
+        if int(user_input) < 0:
             print("Invalid input. Please enter a non-negative number.")
             return 'error'
         else:
-            return user_input
+            return int(user_input)
     else:
-        error += 1
         print("Invalid input. Please enter a number or 'quit' to exit.")
         return 'error'
 
@@ -27,15 +25,16 @@ def calculate_tax(amount):
     return amount*tax
 
 def generate_report(total_units,total_delivered,amount_processed, failed_attempts):
-    print("Total Units Processed:", total_units)
-    print("Total Deliveries Processed:", total_delivered)
-    print("Total Money Processed:", amount_processed)
-    print("Rejected Entries:", failed_attempts)
+    print(f"Total Units Processed: {total_units}")
+    print(f"Total Deliveries Processed: {total_delivered}")
+    print(f"Total Money Processed: ${amount_processed:.2f}")
+    print(f"Rejected Entries: {failed_attempts}")
 
 
 while True:
     uinput = get_valid_input(input("Enter the number of items in inventory: "))
     if uinput == 'error':
+        error += 1
         continue
     elif uinput == "quit":
         generate_report(inventory,deliveries,totalAmount,error)
@@ -43,19 +42,18 @@ while True:
     else:
         if inventory + uinput > 500:
             uinput = 500-inventory
-            print("Warning: Inventory only has a max of 500 units, maximum unit processed:",uinput,'\n')
+            print(f"Warning: Inventory only has a max of 500 units, maximum unit processed: {uinput}\n")
         newcost = uinput*cost
         newtax = calculate_tax(newcost)
         totalcostWithTax = newcost + newtax
         totalAmount = process_delivery(totalAmount,totalcostWithTax)
         print("------------New Delivery------------\n")
-        print("Number of units:",uinput)
-        print("Unit cost before tax:",newcost)
-        print("Delivery Tax(10%):",newtax)
-        print("Total:",totalcostWithTax,'\n')
+        print(f"Number of units: {uinput}")
+        print(f"Unit cost before tax: ${newcost:.2f}")
+        print(f"Delivery Tax(10%): ${newtax:.2f}")
+        print(f"Total: ${totalcostWithTax:.2f}\n")
         inventory += int(uinput)
         deliveries += 1
-        print("Current inventory:", inventory)
+        print(f"Current inventory: {inventory}\n")
         if inventory == 500:
             break
-
